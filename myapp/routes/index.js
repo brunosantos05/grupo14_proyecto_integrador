4b44eca48c6626258e07aa05d683056cd3b1fea5
brunosantos05 <osantos@udesa.edu.ar>
2025-04-13 19:22:31 -0300

    agregue contreoller de mercadoCOntroller y el index.js

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const controlador = require('../controllers/mercadoControllers');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// Página principal
+router.get('/', controlador.index);
+
+// Registro
+router.get('/register', controlador.registro);
+
+// Login
+router.get('/login', controlador.login);
+
+// Perfil del usuario
+router.get('/profile', controlador.perfil);
+
+// Agregar producto
+router.get('/product-add', controlador.productoAgregar);
+
+// Detalle del producto
+router.get('/product', controlador.productoDetalle);
+
+// Resultados de búsqueda
+router.get('/search-results', controlador.resultadosBusqueda);
 
 module.exports = router;
