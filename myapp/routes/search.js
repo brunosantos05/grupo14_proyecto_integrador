da7311b4ab03dc8c6d583a05be12876706d3a69f
Matefascetto <mfascetto@udesa.edu.ar>
2025-04-14 15:58:25 -0300

    agregue remeras

--- myapp/routes/search.js
+++ myapp/routes/search.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const searchController = require('../search/loginController');
+const searchController = require('../controllers/searchController');
 
-router.get(`/`, loginController.index)
+router.get(`/`, searchController.index)
 
 
 module.exports= router
