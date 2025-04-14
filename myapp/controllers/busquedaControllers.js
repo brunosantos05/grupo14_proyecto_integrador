d8b2a4288d870354f2e9910dc24f266b2b9aa937
brunosantos05 <osantos@udesa.edu.ar>
2025-04-14 20:42:26 -0300

    agruegue la busqueda y el partial del footer

--- myapp/controllers/busquedaControllers.js
+++ myapp/controllers/busquedaControllers.js
@@ -1,15 +1,10 @@
+const camisetas = require('../db/products');
+
 const busquedaControllers =  {
   busqueda: function(req, res) {
     const requerimiento = req.query.search;
-    
-    const resultados = [
-
-        { nombreCamiseta: "Camiseta 1", description: "Descripción del producto 1" },
-        { nombreCamiseta: "Camiseta 2", description: "Descripción del producto 2" },
-        { nombreCamiseta: "Camiseta 3", description: "Descripción del producto 3" }
-    ];
-
-    res.render('resultados', { resultados, requerimiento});
+    const productos = camisetas.productos
+    res.render('search-results', {productos:productos});
   }
 };
 
