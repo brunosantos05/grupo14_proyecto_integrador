b5def6b1579917784fb3ccf8b0b01e25ebe4de80
Matefascetto <mfascetto@udesa.edu.ar>
2025-05-29 10:46:59 -0300

    modifique app.js

--- myapp/app.js
+++ myapp/app.js
@@ -3,12 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
 
-const mainRoutes = require('./routes/index');
-const productsRoutes = require('./routes/productRoutes')
-const userRoutes = require('./routes/userRoutes');
-
-const session= require("express-session")
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/userRoutes'); // ← nombre corregido
+var mercadoLibreRouter = require('./routes/productRoutes'); // ← nombre corregido
+const db = require("./database/models");
 
 var app = express();
 
@@ -23,27 +23,52 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  secret: "Nuestro mensaje secreto",
+  secret: "mensaje secreto",
   resave: false,
   saveUninitialized: true
 }));
 
-app.use('/', mainRoutes);
-app.use('/users', userRoutes);
-app.use('/products', productsRoutes);
+// Middleware para pasar el usuario a las vistas si está en sesión
+app.use(function (req, res, next) {
+  if (req.session.user != undefined) {
+    res.locals.user = req.session.user;
+  }
+  return next();
+});
 
-// catch 404 and forward to error handler
+// Middleware para mantener la sesión si hay una cookie
+app.use(function (req, res, next) {
+  if (req.cookies.userId != undefined && req.session.user == undefined) {
+    let idDeLaCookie = req.cookies.userId;
+    db.User.findByPk(idDeLaCookie)
+      .then(function (user) {
+        req.session.user = user;
+        res.locals.user = user;
+        return next();
+      })
+      .catch(function (err) {
+        console.log("error en cookies", err);
+        return next();
+      });
+  } else {
+    return next();
+  }
+});
+
+// Rutas
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/bears', mercadoLibreRouter);
+
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
