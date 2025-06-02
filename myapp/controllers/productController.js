3fb516cdab97dd55e84005507b3cb4b69a31136a
Valentinvallejo5 <vvallejo@udesa.edu.ar>
2025-06-02 19:09:33 -0300

    reestructuracion y agrego nuevas funcionalidades a los controllers

--- myapp/controllers/productController.js
+++ myapp/controllers/productController.js
@@ -1,12 +1,34 @@
-const products = require('../db/products');
+const { Association, where } = require("sequelize");
+const db = require('../database/models');
+const op = db.Sequelize.Op;
+const Product = db.Product;
+
 
 const productoController = {
   detalle: function (req,res) {
     let id = req.params.id
-    let producto = products.productos[id];
-    return res.render('product', {producto:producto});
-  }
-};
+    db.Product.findByPK(id,{
+      include: [
+        { association: "user" },
+        { association: "comentarios"},
+      ]
+    })
+    .then(data => {
+      db.comentarios.findAll({
+        where: { producto_id: data.id},
+        include: [
+          { association: "user"}
+        ]
+      })
+      .then(comentarios => {
+        return res.render("product", {producto: data, comentarios: comentarios})
+      })
+    })
+    .catch(error => {
+      console.log(error);
+  })
+},
+  };
 
 
 module.exports = productoController;
