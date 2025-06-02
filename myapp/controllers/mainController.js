bf20b4dd100d3b49175752f3d6ab23142dc5940a
brunosantos05 <osantos@udesa.edu.ar>
2025-06-02 18:31:40 -0300

    homepage

--- myapp/controllers/mainController.js
+++ myapp/controllers/mainController.js
@@ -1,9 +1,27 @@
-const camisetas = require('../db/products');
+const { Association, where } = require("sequelize");
+const db = require('../database/models');
+const op = db.Sequelize.Op;
+const Product = db.Product;
+
 
 const mainController = {
   index: function (req, res) {
-    return res.render("index", {productos:camisetas.productos});
+    db.Producto.findAll({
+      include: [
+        { association: "user" },
+        { association: "comentarios"},
+  ]
+  })
+  .then(function (data) {
+        console.log(data);
+        return res.render("index", { productos: data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 };
 
+
 module.exports = mainController;
+
